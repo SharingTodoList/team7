9754ec1b5d7ee19f3e8603dfbec5c7f9385210ff
PARKYUNJU <pyunj6767@gmail.com>
2022-06-21 20:56:11 +0900

    todolist말고 mypage에 구현 중

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, jsonify, request, session, redirect, u
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@sparta.xob7d.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
@@ -13,13 +14,14 @@ import datetime
 import hashlib
 from datetime import datetime, timedelta
 
+
 @app.route('/')
 def main():
     return render_template("login.html")
 
+
 @app.route('/register', methods=['POST'])
 def api_register():
-
     id_receive = request.form['username_give']
     pw_receive = request.form['password_give']
     nickname_receive = request.form['nickname_give']
@@ -35,12 +37,14 @@ def api_register():
 
     return jsonify({'result': 'success'})
 
+
 @app.route('/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     # print(username_receive)
     exists = bool(db.member.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
@@ -53,8 +57,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
         }
 
         print(payload)
@@ -62,41 +66,48 @@ def sign_in():
         print(jwt.encode(payload, SECRET_KEY, algorithm='HS256'))
 
         # token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-        #강의에서는 위의코드이나 https://fusiondeveloper.tistory.com/31 에서는 에러가 날때
+        # 강의에서는 위의코드이나 https://fusiondeveloper.tistory.com/31 에서는 에러가 날때
         # 파이썬 버전에 따라 다른데 .decode('utf-8')을 지워줘도 된다고 한다.
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
+
 @app.route('/base')
 def base():
-    return render_template("base.html")
+    return render_template('base.html')
 
 
+@app.route('/todolist')
+def todolist():
+    return render_template('todolist.html')
 
 @app.route('/mypage')
 def mypage():
-    return render_template("mypage.html")
+    return render_template('mypage.html')
 
 
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
     bucket_receive = request.form['bucket_give']
     time_receive=request.form['time_give']
-    bucket_list=list(db.bucket.find({}, {'_id':False}))
+    tag_receive=request.form['tag_give']
+    
+    bucket_list=list(db.todo.find({}, {'_id':False}))
     count= len(bucket_list)+1
 
     doc = {
-
         'seq':count,
-        'detail':bucket_receive,
-        'status':0,
+        'date':datetime.today().strftime("%Y-%m-%d"),
         'time':time_receive,
-        'date':datetime.today()
+        'detail':bucket_receive,
+        'tag':tag_receive,
+        
+        'status':0,
+        
     }
 
     db.todo.insert_one(doc)
@@ -105,27 +116,37 @@ def bucket_post():
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
-    num_receive = request.form['num_give']
-    db.todo.update_one({'num': int(num_receive)}, {'$set': {'status': 1}})
+    seq_receive = request.form['seq_give']
+    db.todo.update_one({'seq': int(seq_receive)}, {'$set': {'status': 1}})
     return jsonify({'msg': '버킷완료!'})
 
 @app.route("/bucket/back",methods=["POST"])
 def bucket_back():
-    num_receive = request.form['num_give']
-    db.todo.update_one({'num': int(num_receive)}, {'$set': {'status': 0}})
+    seq_receive = request.form['seq_give']
+    db.todo.update_one({'seq': int(seq_receive)}, {'$set': {'status': 0}})
     return jsonify({'msg': '버킷취소!'})
+
+@app.route("/bucket/delete",methods=["POST"])
+def bucket_delete():
+    seq_receive = request.form['seq_give']
+    db.todo.delete_one({'seq': int(seq_receive)})
+    return jsonify({'msg': '버킷삭제!'})
 
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
     bucket_list = list(db.todo.find({}, {'_id': False}))
-
+    
     return jsonify({'buckets': bucket_list})
+
+
+
+
+
 
 @app.route('/logout')
 def logout():
     return render_template("logout.html")
-
 
 
 if __name__ == '__main__':
